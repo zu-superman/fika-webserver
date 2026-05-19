ecca90717bb35b856ead81e61f2d28f2fdee23e2
heyi@jbr-remote-dev <heyi@zusf.com>
2026-05-19 14:51:18 +0800

    hotfix: 去除totpSecret配置项的static标记
    refactor: 使用lombok注解代替原来的get/set方法

--- spt-helper-common/src/main/java/cn/hy/common/config/RuoYiConfig.java
+++ spt-helper-common/src/main/java/cn/hy/common/config/RuoYiConfig.java
@@ -1,5 +1,7 @@
 package cn.hy.common.config;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
@@ -10,6 +12,8 @@ import org.springframework.stereotype.Component;
  */
 @Component
 @ConfigurationProperties(prefix = "spt-helper")
+@Getter
+@Setter
 public class RuoYiConfig {
     /** 项目名称 */
     private String name;
@@ -21,61 +25,28 @@ public class RuoYiConfig {
     private String copyrightYear;
 
     /** 上传路径 */
+    @Getter
     private static String profile;
 
     /** 获取地址开关 */
+    @Getter
     private static boolean addressEnabled;
 
     /** 验证码类型 */
+    @Getter
     private static String captchaType;
 
     /**
      * totp秘钥
      */
-    private static String totpSecret;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getVersion() {
-        return version;
-    }
-
-    public void setVersion(String version) {
-        this.version = version;
-    }
-
-    public String getCopyrightYear() {
-        return copyrightYear;
-    }
-
-    public void setCopyrightYear(String copyrightYear) {
-        this.copyrightYear = copyrightYear;
-    }
-
-    public static String getProfile() {
-        return profile;
-    }
+    private String totpSecret;
 
     public void setProfile(String profile) {
         RuoYiConfig.profile = profile;
     }
 
-    public static boolean isAddressEnabled() {
-        return addressEnabled;
-    }
-
     public void setAddressEnabled(boolean addressEnabled) {
         RuoYiConfig.addressEnabled = addressEnabled;
-    }
-
-    public static String getCaptchaType() {
-        return captchaType;
     }
 
     public void setCaptchaType(String captchaType) {
