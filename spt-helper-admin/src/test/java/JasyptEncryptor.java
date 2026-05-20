15dfeabdc072a062677915321d8d44025b630af9
esxi-fika <ppshy@foxmail.com>
2026-05-20 21:31:54 +0800

    config(data): 更新fika环境配置文件中的数据库连接信息和加密凭据

--- spt-helper-admin/src/test/java/JasyptEncryptor.java
+++ spt-helper-admin/src/test/java/JasyptEncryptor.java
@@ -12,10 +12,10 @@ public class JasyptEncryptor {
         encryptor.setAlgorithm("PBEWithMD5AndDES");
 
         System.out.println("db user: " + encryptor.encrypt("shs-adm"));
-        System.out.println("db pwd: " + encryptor.encrypt("Y5huY8xcsvQmuc2P33UYP3nS8Sun8uER"));
+        System.out.println("db pwd: " + encryptor.encrypt("jGubYzQ2rpcUXP4sBhEYWzcb2fJJUp4Y"));
         System.out.println("druid user: " + encryptor.encrypt("singleDog"));
-        System.out.println("druid pwd: " + encryptor.encrypt("RR4EPEBmVZEZHwhsf2MU7BTnRAns3G3N"));
-        System.out.println("redis pwd: " + encryptor.encrypt("D060VmIBwG"));
-        System.out.println("token secret: " + encryptor.encrypt("fnKMSUmQPzYjcYfKcrYBhxUWA3ur5VRC"));
+        System.out.println("druid pwd: " + encryptor.encrypt("TVCn3GGQ5Z2ZbtPwxJ5xASFtnNjSmXYu"));
+        System.out.println("redis pwd: " + encryptor.encrypt("ePKznPKHdCY8GaSSfY3FxUWfRp5QecHn"));
+        System.out.println("token secret: " + encryptor.encrypt("zZYCWGteHRDt68mRWYH26a6xwd8R4HYn"));
     }
 }
